ceeb4110996e8979029043f77a6895006713826f
kanchan3d <kanchan.dasila1@gmail.com>
2025-03-17 23:24:26 +0530

    Added Functionalities

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
+	const {navigate, isEducator}=useContext(AppContext);
 	const isCourseListPage = location.pathname.includes("/course-list");
 
 	const { openSignIn } = useClerk();
@@ -15,6 +17,7 @@ const Navbar = () => {
 				isCourseListPage ? "bg-white" : "bg-cyan-100/70"
 			}`}>
 			<img
+				onClick={() => navigate("/")}
 				src={assets.logo}
 				alt="Logo"
 				className="w-28 lg:w-32 cursor-pointer"
@@ -23,7 +26,7 @@ const Navbar = () => {
 				<div className="flex items-center gap-5">
 					{user && (
 						<>
-							<button>Become Educator</button>
+							<button onClick={()=>{navigate('/educator')}}>{isEducator?'Educator Dashboard':'Become Educator'}</button>
 							<Link to="/my-enrollments">My Enrollments</Link>
 						</>
 					)}
@@ -43,7 +46,7 @@ const Navbar = () => {
 				<div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
         {user && (
 						<>
-							<button>Become Educator</button>
+							<button onClick={()=>{navigate('/educator')}}>{isEducator?'Educator Dashboard':'Become Educator'}</button>
 							<Link to="/my-enrollments">My Enrollments</Link>
 						</>
 					)}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,21 +1,36 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
     const currency=import.meta.env.VITE_CURRENCY;
+    const navigate=useNavigate();
 
     const [allCourses,setAllCourses]=useState([]);
+    const [isEducator,setIsEducator]=useState(true);
     //fetch all courses
     const fetchAllCourses=async()=>{
         setAllCourses(dummyCourses);
     }
+
+    // function to calculate rating
+    const calculateRating=(course)=>{
+        if(course.courseRatings.length===0){
+            return 0;
+        }
+        let total=0;
+        course.courseRatings.forEach(rating=>total+=rating.rating);
+        return total/course.courseRatings.length;
+    }
+
+
     useEffect(()=>{
         fetchAllCourses();
     }
     ,[])
-    const value={currency, allCourses}
+    const value={currency, allCourses, navigate, calculateRating, isEducator, setIsEducator};
     return (
         <AppContext.Provider value={value}>
            { props.children}
