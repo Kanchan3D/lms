0ae6d99f2311bb2308dadaa7911c0248f5e5beca
kanchan3d <kanchan.dasila1@gmail.com>
2025-03-22 21:02:05 +0530

    Course Details Fetching

--- client/src/components/student/CourceCard.jsx
+++ client/src/components/student/CourceCard.jsx
@@ -12,7 +12,7 @@ const CourceCard = ({course}) => {
       src={course.courseThumbnail} alt="" />
       <div className='p-3 text-left'>
         <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
-        <p className='text-gray-500'>{course.educator.name}</p>
+        <p className='text-gray-500'>Kanchan Dasila</p>
         <div className='flex items-center space-x-2'>
           <p>{calculateRating(course)}</p>
           <div className='flex'>{[...Array(5)].map((_,i)=>(
