0b8e5d2ce432ff9b3c3d05cd860dd69aebb17cfe
kanchan3d <kanchan.dasila1@gmail.com>
2025-03-26 16:50:54 +0530

    fixed some issues

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
+import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 
 const Educator = () => {
-  return (
-    <div>
-      <h1>educator</h1>
-      <div>
-        {<Outlet/>}
-      </div>
-    </div>
-  )
-}
+	return (
+    <>
+		<div className="text-default min-h-screen bg-white">
+			<Navbar />
+			<div className="flex">
+				<Sidebar />
+				<div className="flex-1">{<Outlet />}</div>
+			</div>
+		</div>
+    <Footer/>
+    </>
+	);
+};
 
-export default Educator
+export default Educator;
